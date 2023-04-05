dfb5b023764c3d81c3d984f8c79d853dfff93e1b
Tony Dandino <tdandino1005@gmail.com>
2023-04-04 23:38:01 -0500

    Started controllers

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,10 @@
 const {User, Thought} = require('../models');
 
+// get all users
 module.exports = {
-    postUser(req, res) {
+    getUser(req, res) {
+        User.find({})
+        .then((users) => res.json(users))
+        .catch((err) => res.json(err));
+    },
+
