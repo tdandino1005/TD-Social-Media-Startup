5ba5553c5d079c589262d49d31942bebc425a9e6
Tony Dandino <tdandino1005@gmail.com>
2023-04-28 21:10:06 -0500

    Fixing connection

--- config/connection.js
+++ config/connection.js
@@ -2,10 +2,10 @@
 const mongoose = require('mongoose');
 
 
-mongoose.connect('mongodb://localhost:27017/socialmediadb', {
+mongoose.connect('mongodb://127.0.0.1:27017/socialmediadb', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 
 })
 
-module.exports = connection;
+module.exports = mongoose.connection;
