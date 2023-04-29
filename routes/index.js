b00bf86cd66af291957cfd40fc39d913ad659644
Tony Dandino <tdandino1005@gmail.com>
2023-04-28 21:19:12 -0500

    Fixing connection problem

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,14 @@
 // connect to the database
 const router = require('express').Router();
-const userRoutes = require('./userRoutes');
-const thoughtRoutes = require('./thoughtRoutes');
+const apiRoutes = require('./api');
 
-// connect to the routes
-router.use('/users', userRoutes);
-router.use('/thoughts', thoughtRoutes);
+// connect to the api routes
+router.use('/api', apiRoutes);
+
+// if the route is not found, send a 404 error
+router.use((req, res) => res.send("<h1>Wrong Route!</h1>"));
+
+
 
 // export the router
 module.exports = router;
